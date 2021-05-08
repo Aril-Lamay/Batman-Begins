0f7141f99bd3a7c877f0dc915b112ff8053de65f
Aril-Lamay <72742540+Aril-Lamay@users.noreply.github.com>
2021-05-08 14:46:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+var drop;
+var drops = [];
+var maxDrops = 100;
+
+var umbrella;
+
+var rand;
+var thunder, thunder1, thunder2, thunder3, thunder4;
+
+function preload(){
+    thunder1 = loadImage("images/thunderbolt/1.png");
+    thunder2 = loadImage("images/thunderbolt/2.png");
+    thunder3 = loadImage("images/thunderbolt/3.png");
+    thunder4 = loadImage("images/thunderbolt/4.png");
+}
+
+function setup(){
+    var canvas = createCanvas(400,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    umbrella = new Umbrella(200,400);
+
+    if(frameCount%150 === 0){
+        for (var i =0;i<maxDrops;i++){
+            drops.push(new Drop(random(0,400),random(0,400),5));
+        }
+    }
+   
+    rand = Math. round (random(1, 4) ) ;
+}
+
+function draw(){
+
+    Engine.update(engine);
+
+    background('Black');
+
+    for(var i = 0;i<maxDrops;i++){
+        drops[i].display();
+        drops[i].update();
+    }   
+
+    if (frameCount%80===0) {
+        thunderCreatedFrame=frameCount;
+        thunder = createSprite(random(10, 370), random(10, 30), 10, 10);
+        switch (rand) {
+
+            case 1: thunder . addImage (thunder1) ;
+            break;
+            case 2: thunder . addImage (thunder2);
+            break;
+            case 3: thunder . addImage (thunder3) ;
+            break;
+            case 4: thunder . addImage (thunder4);
+            break;
+            default: break;
+        }
+        thunder . scale = random(0.3, 0.8);
+    }
+    
+    if(frameCount%92 ===0){
+        thunder.destroy();
+    }
+
+    
+    drawSprites();
+
+    umbrella.display();
+}
+
